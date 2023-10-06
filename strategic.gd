a84b2c33c219ba7854399d7a125937b154299c0c
= <l351s450@ku.edu>
2023-10-05 20:44:52 -0500

    pause menu initial push
    Initial push for pause menu functionality (not working correctly yet)
    - Pause menu appears when escape is pressed after starting a new game, but it does not pause the game
    - Pause menu needs to be repositioned to center of the screen

--- strategic.gd
+++ strategic.gd
@@ -2,8 +2,13 @@ extends Control
 
 export var PlayerBASE: PackedScene
 
+#get our PauseMenu scene as an object
+onready var pause_menu = $PauseMenu
+var game_paused = false #boolean for status of pause 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pause_menu.hide() #make sure paused menu is not visible when the game starts
 	pass # Replace with function body.
 
 
@@ -18,4 +23,19 @@ func _process(delta):
 		  #base.initialize(position, position)
 		#add_child(base)
 		pass
-
+	
+	#Pause Menu Code
+	if(Input.is_action_just_pressed("pause")):
+		pauseMenu()
+		
+func pauseMenu(): #should trigger every time the "pause" event takes place (i.e. every time "Esc" is pressed)
+	if game_paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+		
+	game_paused = !game_paused
+		
+		
