dec9fd969909bce62b172180e39bbd550dec9b68
Sid <104640842+Siddharth24112001@users.noreply.github.com>
2023-10-08 17:41:01 -0500

    Update Gameplay.gd

--- Gameplay.gd
+++ Gameplay.gd
@@ -1,27 +1,45 @@
 extends Node2D
 
 var laser_scene:PackedScene = preload("res://Laser/laser.tscn")
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var drone_scene: PackedScene = preload("res://Drone/Drone.tscn")
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+#	 $Drone.hide()
+	 $Drone_Timer.start()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 
-
-
-
-
-func _on_PLayer_laser():
+func _on_PLayer_laser(pos, direction):
 	var laser = laser_scene.instance() 
-	laser.position = $PLayer.global_position
-	add_child(laser)
+	laser.position = pos 
+	laser.rotation_degrees = rad2deg(direction.angle()) + 90
+	laser.global_position = $Player/LaserStartPositions.global_position
+	laser.direction = direction
+	$Laser.add_child(laser)
 
+func _on_Drone_Timer_timeout():
+	spawn_drone()
+	
+func spawn_drone():
+	var new_drone = drone_scene.instance()
+	var screen_size = get_viewport_rect().size
+	new_drone.position.x = screen_size.x
+	new_drone.position.y = screen_size.y / 2
+	$Drone.add_child(new_drone)
+	
+	# Connect the signal from the new drone instance
+	new_drone.connect("laser", self, "_on_Drone_laser")
+	
+
+func _on_Drone_laser(pos,direction):
+	var drone_laser = laser_scene.instance()
+	drone_laser.position = pos
+	drone_laser.rotation_degrees = rad2deg(direction.angle()) + 90
+	drone_laser.global_position = Globals.drone_laser_pos
+	drone_laser.direction = direction
+	$Laser.add_child(drone_laser)
+	
+	
+	
