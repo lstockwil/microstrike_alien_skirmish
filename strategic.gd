2ee7051625bb2dc37b04f88e94e751022b2bda55
= <l351s450@ku.edu>
2023-10-20 15:16:55 -0500

    time system prototype
    Game time prints every 100 cycles of "_process" function within strategic.gd.
    
    Need to link game time to the rest of the game classe

--- strategic.gd
+++ strategic.gd
@@ -6,18 +6,30 @@ export var PlayerBASE: PackedScene
 onready var pause_menu = $PauseMenu
 var game_paused = false #boolean for status of pause 
 
+onready var time_constant = 100
+onready var counter = 0
+onready var game_time = 0
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pause_menu.hide() #make sure paused menu is not visible when the game starts
+	pause_menu.hide() #make sure pause menu is not visible when the game starts
 	pass # Replace with function body.
 
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	#update time
+	counter = (counter + 1) % time_constant
+	if(counter == 0): #every *time constant* cycles of process, the in-game time is increased by one unit
+		game_time += 1
+		print(game_time)
+	
+	
 	#track player mouse movement
 	var position = get_global_mouse_position()
 	var offset = get_local_mouse_position()
-	
+
 	if (Input.is_action_pressed("mouse_click")):
 		#var base = PlayerBASE.instance()
 		  #base.initialize(position, position)
@@ -28,6 +40,8 @@ func _process(delta):
 	if(Input.is_action_just_pressed("pause")):
 		pauseMenu()
 		
+		
+#PauseMenu() function only shows and hides menu, doesn't pause game right now: revisit once global time is implemented
 func pauseMenu(): #should trigger every time the "pause" event takes place (i.e. every time "Esc" is pressed)
 	if game_paused:
 		pause_menu.hide()
