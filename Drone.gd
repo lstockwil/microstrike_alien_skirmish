c7535d36faa133fd47c024a7f5e59bf43c5b2974
Sid <104640842+Siddharth24112001@users.noreply.github.com>
2023-10-26 23:09:53 -0500

    Update Drone.gd

--- Drone.gd
+++ Drone.gd
@@ -8,12 +8,14 @@ var health = 50
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	#position of player minus enemy position will give us direction
-	$Drone_image.look_at(Globals.player_pos)
-	$Drone_image.rotation_degrees += 90
+	look_at(Globals.player_pos)
+	$Drone_image.rotation_degrees = 90
 	var player_position = Globals.player_pos
-	var target_position = (player_position - position).normalized()
+	var target_position = (player_position - global_position).normalized()
 	var laser_position = $Laser_Position.global_position
 	Globals.drone_laser_pos = $Laser_Position.global_position
+	
+	
 	if can_laser:
 		emit_signal("laser", laser_position, target_position)
 		can_laser = false
@@ -38,3 +40,11 @@ func _on_Laser_Timer_timeout():
 	can_laser = true
 	
 	
+func die():
+	$AnimationPlayer.play("Explosion")
+	$Laser_Timer.stop()
+
+func hit():
+	health-=10
+	if health<=0:
+		die()
