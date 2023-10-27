46e2877fa4d7dbbeed6d2bf310d664444cc54fc5
Sid <104640842+Siddharth24112001@users.noreply.github.com>
2023-10-26 23:15:03 -0500

    Update Player.gd

--- Player.gd
+++ Player.gd
@@ -2,28 +2,40 @@ extends KinematicBody2D
 
 signal laser(pos,direction)
 	
-var speed: int = 500
+var speed: int = 250
 var can_laser: bool = true
+var health: int = 50
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	Globals.player_pos = global_position
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var velocity = direction * speed
 	move_and_slide(velocity)
-	Globals.player_pos = $Player_image.global_position
+	Globals.player_pos = global_position
 	
 	
 	#rotate player
 	look_at(get_global_mouse_position())
-	var player_direction = (get_global_mouse_position() -position).normalized()
+	var player_direction = (get_global_mouse_position() - position).normalized()
+	
+	#shoot laser on space
 	if Input.is_action_pressed("Shoot_laser") and can_laser:
+#		Set position of laser initiation
 		var laserPosition = $LaserStartPositions
+		
 		can_laser = false
 		$Laser_timer.start()
 		emit_signal("laser", laserPosition.global_position, player_direction)
+
 func _on_Timer_timeout():
 	can_laser = true
 
 
+func die():
+	queue_free()
 
-	
+func hit():
+	health -= 10
+	if health<=0:
+		die()
